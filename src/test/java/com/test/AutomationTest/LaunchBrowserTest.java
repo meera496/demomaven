af8c754f6a91bfa6344cf0696f509e6042f8b979
meera496 <meeravali.vtest@gmail.com>
2017-06-08 18:56:54 +0530

    test is beguin

---
+++ src/test/java/com/test/AutomationTest/LaunchBrowserTest.java
@@ -0,0 +1,14 @@
+package com.test.AutomationTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowserTest {
+@Test
+public static void method(){
+	WebDriver driver=new FirefoxDriver();
+	
+	driver.get("http://www.google.com");
+}
+}
